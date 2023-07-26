571656a180307ff42eff5249ee0e39110924060f
XXXXsonjisung <jklasd9909@gmail.com>
2023-07-26 16:13:07 +0900

    메세제

--- 07_ObjectArray_Homework_sonjisung/src/com/hw1/model/vo/Employee/ClassDiagram.java
+++ 07_ObjectArray_Homework_sonjisung/src/com/hw1/model/vo/Employee/ClassDiagram.java
@@ -1,5 +1,8 @@
 package com.hw1.model.vo.Employee;
 
 public class ClassDiagram {
+	//필드
+	
+	
 
 }
--- 07_ObjectArray_Homework_sonjisung/src/com/hw1/run/Run.java
+++ 07_ObjectArray_Homework_sonjisung/src/com/hw1/run/Run.java
@@ -1,5 +1,15 @@
 package com.hw1.run;
 
+import com.hw1.model.vo.Employee.ClassDiagram;
+
 public class Run {
+	public static void main(String[] args) {
+		
+		
+		ClassDiagram ClassDi = new ClassDiagram();
+		
+		ClassDi.equals(ClassDi);
+		
+	}
 
 }
